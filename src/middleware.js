20098b48d481b891ff63d16baa1d709a133aa0ed
Walid <besswalid@gmail.com>
2025-03-24 11:57:10 -0400

    [FIX]

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,9 @@
 import { NextResponse } from "next/server";
 export async function middleware(request) {
+  const { pathname } = request.nextUrl
+  console.log('Pathname', pathname);
+  if (pathname === '/') {
+    return NextResponse.redirect(new URL('sign-in', request.url))
+  }
   return NextResponse.next()
 }

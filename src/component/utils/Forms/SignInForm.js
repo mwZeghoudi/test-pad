eb724cfdb8ec04d2046c7ae579bc82ba823c4bac
iLnsiC <besswalid@gmail.com>
2025-03-26 16:56:27 +0100

    [FIX] Message

--- src/component/utils/Forms/SignInForm.js
+++ src/component/utils/Forms/SignInForm.js
@@ -1,35 +1,40 @@
-'use client'
-import { useActionState, useEffect } from 'react'
-import { useRouter } from 'next/navigation';
-import { z } from "zod"
-import FormComponent from "../FormComponent"
-import { Checkbox } from "@/components/ui/checkbox"
-import FormLink from "../FormLink"
-import { login } from "@/@/lib/auth"
+"use client";
+import { useActionState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+import FormComponent from "../FormComponent";
+import { Checkbox } from "@/components/ui/checkbox";
+import FormLink from "../FormLink";
+import { login } from "@/@/lib/auth";
 export default function SignInForm() {
   const router = useRouter();
-  const [state, formAction, pending] = useActionState(login, { success: null, errors: {} })
+  const [state, formAction, pending] = useActionState(login, {
+    success: null,
+    errors: {},
+  });
   const formObject = [
     {
-      name: 'email',
-      placeholder: 'Email',
-      type: 'email',
+      name: "email",
+      placeholder: "Email",
+      type: "email",
       validation: z.string().email({ message: "Invalid email address" }),
-      defaultValue: ''
+      defaultValue: "",
     },
     {
-      name: 'password',
-      placeholder: 'password',
-      type: 'password',
-      validation: z.string().min(8, { message: "Username must be at least 8 characters." }),
-      defaultValue: ''
-    }
-  ]
+      name: "password",
+      placeholder: "password",
+      type: "password",
+      validation: z
+        .string()
+        .min(8, { message: "Username must be at least 8 characters." }),
+      defaultValue: "",
+    },
+  ];
   useEffect(() => {
     if (state.success) {
-      router.push('/dashboard');
+      router.push("/dashboard");
     }
-  }, [state])
+  }, [state]);
 
   return (
     <FormComponent
@@ -39,15 +44,17 @@ export default function SignInForm() {
       beforeSubmit={
         <div className="flex items-center space-x-2 justify-between">
           <div className="flex items-center space-x-2">
-            < Checkbox id="remember" />
+            <Checkbox id="remember" />
             <label
               htmlFor="remember"
               className="leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >Remember me</label>
+            >
+              Remember me
+            </label>
           </div>
           <FormLink href="reset-password" label="Forgot password ?" />
         </div>
       }
     />
-  )
+  );
 }
